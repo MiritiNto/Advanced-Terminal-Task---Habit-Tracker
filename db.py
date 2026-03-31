dddf0c99dc84f02dad641d9376d19d43e8e336c9
Miriti Elijah <miritinto@gmail.com>
2026-03-31 23:41:31 +0300

    sqlite connection and base tasks table

---
+++ db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+DB_NAME = "tracker.db"
+
+def get_connection():
+    return sqlite3.connect(DB_NAME)
+
+def init_db():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                status TEXT DEFAULT 'pending'
+            )
+        ''')
+        conn.commit()
