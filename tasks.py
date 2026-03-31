8eb7f3be2c1b395a48773181e3911c86f54cbf94
Miriti Elijah <miritinto@gmail.com>
2026-03-31 23:47:34 +0300

    task retrieval and listing

--- tasks.py
+++ tasks.py
@@ -6,3 +6,10 @@ def add_task(title):
         cursor.execute("INSERT INTO tasks (title) VALUES (?)", (title,))
         conn.commit()
     print(f"Task added: '{title}'")
+
+def list_tasks():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, title, status FROM tasks")
+        for row in cursor.fetchall():
+            print(f"[{row[0]}] {row[1]} - {row[2].upper()}")
