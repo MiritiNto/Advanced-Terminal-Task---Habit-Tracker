c6a7ee97e1b0ce92866476552585b39370f53765
Miriti Elijah <miritinto@gmail.com>
2026-03-31 23:49:32 +0300

    connect list tasks functionality to CLI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 from db import init_db
-from tasks import add_task
+from tasks import add_task, list_tasks
 
 def main():
     init_db()
@@ -10,10 +10,14 @@ def main():
     parser_add = subparsers.add_parser("add", help="Add a new task")
     parser_add.add_argument("title", type=str, help="Task title")
     
+    parser_list = subparsers.add_parser("list", help="List all tasks")
+    
     args = parser.parse_args()
     
     if args.command == "add":
         add_task(args.title)
+    elif args.command == "list":
+        list_tasks()
     elif not args.command:
         parser.print_help()
 
