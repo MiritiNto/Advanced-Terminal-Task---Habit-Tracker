af9be264289940370e90835b105167e7155b3112
Miriti Elijah <miritinto@gmail.com>
2026-04-10 23:09:36 +0300

    sqlite db backup

--- utils.py
+++ utils.py
@@ -1,16 +1,16 @@
 import csv
 import json
-from db import get_connection
+import shutil
+from db import get_connection, DB_NAME
 
 def export_tasks_csv(filename="tasks.csv"):
     with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM tasks")
         rows = cursor.fetchall()
-        
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(["ID", "Title", "Status", "Priority"])
+        writer.writerow(["ID", "Title", "Status", "Priority", "Due Date"])
         writer.writerows(rows)
     print(f"Exported to {filename}")
 
@@ -19,8 +19,11 @@ def export_tasks_json(filename="tasks.json"):
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM tasks")
         rows = cursor.fetchall()
-        
-    data = [{"id": r[0], "title": r[1], "status": r[2], "priority": r[3]} for r in rows]
+    data = [{"id": r[0], "title": r[1], "status": r[2]} for r in rows]
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
     print(f"Exported to {filename}")
+
+def backup_db():
+    shutil.copyfile(DB_NAME, f"{DB_NAME}.bak")
+    print("Database backed up successfully.")
